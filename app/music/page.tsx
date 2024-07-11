e3f6fd8f2fd43137e924db38c8173cec03886087
upon-X <valemiche003@gmail.com>
2024-07-11 20:09:59 -0300

    intializing routes

---
+++ app/music/page.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "@/components/Navbar/Navbar";
+
+export default function Music() {
+  return (
+    <section color="white">
+      <Navbar />
+      <h2>Music</h2>
+      <p>These are the latest songs</p>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,38 +4,35 @@ import { Navbar } from "@/components/Navbar/Navbar";
 import { Posts } from "@/components/Posts";
 
 export default function Home() {
-  const logoBand2 =
+  const logoBandNoBG =
+    "https://res.cloudinary.com/dzhiauyws/image/upload/v1720739011/Imagine%20Dragons/imaginedragons_jufa4y2_atftnq.webp";
+  const logoBand =
     "https://res.cloudinary.com/dzhiauyws/image/upload/v1720537082/Imagine%20Dragons/imaginedragons_jufa4y.webp";
   const BandMembers =
     "https://res.cloudinary.com/dzhiauyws/image/upload/v1720556759/Imagine%20Dragons/imaginedragonsband_ictzde.webp";
 
   return (
     <main className={styles.main}>
-      {/* <Image
-        className={styles.dan_reynolds_image}
-        src={DanReynolds}
-        width={450}
-        height={450}
-        alt={"Dan Reynolds"}
-      /> */}
       <header className={styles.header}>
         <Image
           className={styles.band_image}
           src={BandMembers}
-          width={1100}
-          height={600}
+          width={1000}
+          height={550}
           alt="Imagine Dragons Band"
         />
         <section className={styles.title_image}>
           <Image
             className={styles.logo_band}
-            src={logoBand2}
+            src={logoBandNoBG}
             width={400}
             height={400}
             alt={"Logo Imagine Dragons"}
           />
           <h1 className={styles.title}>
-            The official fanpage for Firebreathers around the world
+            The official fanpage for{" "}
+            <span className={styles.title_span}>Firebreathers</span> around the
+            world
           </h1>
         </section>
       </header>
