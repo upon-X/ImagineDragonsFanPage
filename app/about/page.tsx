4321785afb629fb085f9a0c33defaf25d15ddc1d
upon-X <valemiche003@gmail.com>
2024-08-22 12:27:06 -0300

    new styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,19 +1,23 @@
 import { Navbar } from "@/components/Navbar/Navbar";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 /* eslint-disable react/no-unescaped-entities */
 export default function About() {
   return (
-    <section style={{ color: "white" }}>
+    <>
       <Navbar />
-      <h1>About</h1>
-      <p>
-        Imagine Dragons is an American pop rock band from Las Vegas, Nevada,
-        consisting of lead vocalist Dan Reynolds, guitarist Wayne Sermon,
-        bassist Ben McKee, and drummer Daniel Platzman. The band first gained
-        exposure with the release of single "It's Time", followed by their
-        award-winning debut studio album Night Visions (2012), which resulted in
-        the chart-topping singles "Radioactive" and "Demons".
-      </p>
-    </section>
+      <section className={styles.about_section} style={{ color: "white" }}>
+        <h1>About</h1>
+        <p className={styles.p1}>
+          Imagine Dragons is an American pop rock band from Las Vegas, Nevada,
+          consisting of lead vocalist Dan Reynolds, guitarist Wayne Sermon,
+          bassist Ben McKee, and drummer Daniel Platzman. The band first gained
+          exposure with the release of single "It's Time", followed by their
+          award-winning debut studio album Night Visions (2012), which resulted
+          in the chart-topping singles "Radioactive" and "Demons".
+        </p>
+      </section>
+    </>
   );
 }
